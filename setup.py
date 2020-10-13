d3d7019df6c693be95b2bba814e3f1239f4cbe96
Florian Lämmlein <57487327+Flory-1@users.noreply.github.com>
2020-10-13 21:53:40 +0200

    updated setup

--- setup.py
+++ setup.py
@@ -1,31 +1,16 @@
 import re
-import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+from setuptools import setup
 
-with open("src/__init__.py", encoding="utf8") as f:
+with open("src/flask_dtable/__init__.py", encoding="utf8") as f:
     version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
 
-setuptools.setup(
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+setup(
     name='flask_dtable',
     version=version,
-    author='Florian Lämmlein',
-    author_email='florian.leammlein@gmail.com',
-    description="Creates and returns an HTML table",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url='https://github.com/Flory-1/flask_dtable',
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Framework :: flask_dtable",
-        "Programming Language :: Python",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
     install_requires=[
-
+        "Flask"
     ],
     python_requires='>=3.6',
 )
